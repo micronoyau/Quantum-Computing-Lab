02e4485a53f92e1807a938a8e87030d63f825e9b
heureuxpoulpe <leo.lf.dk@gmail.com>
2021-12-09 13:30:13 +0100

    Added Steane code

--- QEC/with_cirq/5qbit.py
+++ QEC/with_cirq/5qbit.py
@@ -17,12 +17,12 @@ X_bar = np.array([[0,0,0,0,1, 1,0,0,1,0],])
 
 Z_bar = np.array([[0,0,0,0,0, 1,1,1,1,1],])
 
-sc = StabilizerCode (5, 1, S, H, Z_bar, X_bar, debug=True)
+sc = StabilizerCode (5, 1, S, H, Z_bar, X_bar, debug=False)
 
-#sc.qc.append (cirq.X(sc.qreg[1]))
+sc.qc.append (cirq.X(sc.qreg[0]))
 
 #sc.operator (X_bar, 0)
 
-#sc.check_generators ()
-#sc.measure_logical ()
-print(sc.qc)
+sc.check_generators ()
+sc.measure_logical ()
+#print(sc.qc)
